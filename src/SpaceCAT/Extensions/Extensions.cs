4beb03206653eac4a942c09fbbc4c63ab84602e1
kevinonfrontend <kevinonfrontend@outlook.com>
2023-06-19 20:57:24 +0100

    rename parameters

--- src/SpaceCAT/Extensions/Extensions.cs
+++ src/SpaceCAT/Extensions/Extensions.cs
@@ -9,15 +9,15 @@ public static class Extensions
     public static void AddSpaceCATClient(this IServiceCollection services)
     {
         var serviceProvider = services.BuildServiceProvider();
-        var tibetSwapOptions = serviceProvider.GetRequiredService<IOptions<SpaceScanOptions>>()?.Value;
-        if (tibetSwapOptions == null)
+        var spaceScanOptions = serviceProvider.GetRequiredService<IOptions<SpaceScanOptions>>()?.Value;
+        if (spaceScanOptions == null)
             throw new ArgumentException("SpaceCAT Configuration section missing!");
-        if (string.IsNullOrEmpty(tibetSwapOptions?.ApiEndpoint))
+        if (string.IsNullOrEmpty(spaceScanOptions?.ApiEndpoint))
             throw new ArgumentException("SpaceCAT.ApiEndpoint not defined");
         
         services.AddHttpClient<ISpaceCATClient, SpaceCATClient>(c =>
         {
-            c.BaseAddress = new System.Uri(tibetSwapOptions.ApiEndpoint);
+            c.BaseAddress = new System.Uri(spaceScanOptions.ApiEndpoint);
         });
     }
 }
